53fec05d70115cc85938c50cef4ae635aba44765
swapnil200419 <126847631+swapnil200419@users.noreply.github.com>
2023-04-11 17:36:18 +0000

    main

---
+++ problem_2_a.py
@@ -0,0 +1,19 @@
+'''
+Conversion: Input some numbers, do some simple arithmetic to do silly conversions(Python3) 
+'''
+
+a=float(input("Enter distance in Kilometers: "))
+b=a*100000
+print(b,"cm")
+c=a*1000
+print(c,"meters")
+d=float(input("Enter the weight of oil in Kilograms: "))
+e=d*1000
+print(e,"grams")
+f=d*1.1364
+print(f,"litres")
+g=float(input("Enter time in hours: "))
+h=g*60
+print(h,"minutes")
+i=g*3600
+print(i,"seconds")
